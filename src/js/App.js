660d49450e7c2fc0e3cd32476eb2d338eb213ce8
Velineni Bhavana <velinenibhavana@gmail.com>
2020-06-12 00:06:33 +0530

    Fix pagination and styles

--- src/js/App.js
+++ src/js/App.js
@@ -9,51 +9,47 @@ import Button from 'react-bootstrap/Button';
 import Alert from 'react-bootstrap/Alert';
 import Spinner from 'react-bootstrap/Spinner';
 import Card from 'react-bootstrap/Card';
-import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
+import FormGroup from 'react-bootstrap/FormGroup';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 const App = (props) => {
 
   const [searchString, updateSearchString] = useState();
   const [loading, updateLoading] = useState(false);
-  const [pageNo, updatePageNo] = useState(1);
+  const [pageNo, updatePageNo] = useState(0);
 
   const [perPage] = useState(20);
 
-  const handleChange = event => {
-    event.preventDefault();
-    updateSearchString(event.target.value);
-  }
-
-  const handleSubmit = event => {
-    if (event) {
-      event.preventDefault();
-    }
+  useEffect(() => {
     updateLoading(true);
     (async () => {
       await props.actions.getUserList(searchString, perPage, pageNo);
       updateLoading(false);
     })();
-  }
+  }, [searchString, pageNo]);
 
-  const handleClick = () => {
-    event.preventDefault();
-    updatePageNo(pageNo + 1);
-    handleSubmit();
+  const handleSubmit = event => {
+    if (event.key === "Enter") {
+      updatePageNo(1);
+      updateSearchString(event.target.value);
+    }
   }
 
   return (
     <Container>
       <Row className="justify-content-md-center">
         <Col lg="5">
-          <Form onSubmit={handleSubmit}>
-            <FormControl
-              type="text"
-              className="searchInput"
-              placeholder="Search for an user"
-              onChange={event => handleChange(event)}
-            />
-          </Form>
+          <FormGroup>
+            <InputGroup>
+              <FormControl
+                type="input"
+                className="searchInput"
+                placeholder="Search for an user"
+                onKeyPress={event => handleSubmit(event)}
+              />
+            </InputGroup>
+          </FormGroup>
         </Col>
       </Row>
       {!loading ?
@@ -78,9 +74,16 @@ const App = (props) => {
               Your seach for <b>'{searchString}'</b> returned <b>{props.usersData.total_count}</b> matches.
     </Alert> : null}
           </Row></> : <Spinner animation="border" />}
-          <Row>{props.usersData && props.usersData.total_count > perPage ?
-            <Button className='button-next' onClick={handleClick}> Next </Button> : null}
-            </Row>
+      <Row>
+        <Col>
+          {props.usersData && pageNo > 1 ?
+            <Button className='button-prev' onClick={() => { updatePageNo(pageNo => pageNo - 1); }} > Prev </Button> : null}
+        </Col>
+        <Col>
+          {props.usersData && props.usersData.total_count > perPage ?
+            <Button className='button-next' onClick={() => { updatePageNo(pageNo => pageNo + 1); }} > Next </Button> : null}
+        </Col>
+      </Row>
     </Container>
   );
 };
